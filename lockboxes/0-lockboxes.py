689877b2d88cd02e1fa15356d5b58ecd8bd83a96
Elizbeh <eliz01_01@yahoo.com>
2023-03-26 05:01:18 +0200

    Add documentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python3
 def canUnlockAll(boxes):
+  # Boxes to be opened
     boxes_opened = {0}
-    
+    # Boxes  that needs to be opend
     next_to_open = [0]
-    
+    #Interattion until all boxes  have been opened
     while len(next_to_open) > 0:
         box = next_to_open.pop(0)
-        
+        #Loop over the keys in the current box
         for key in boxes[box]:
+          # Add newly open box to the queue
           if key not in boxes_opened and key < len(boxes):
                 next_to_open.append(key)
                 boxes_opened.add(key)
     
+    # Return True, else return False if all boxes has been opened
     return len(boxes_opened) == len(boxes)
