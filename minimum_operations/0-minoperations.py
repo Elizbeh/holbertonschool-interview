9828d99c0de443f78d2bf5434fd105beb513bb83
Elizbeh <eliz01_01@yahoo.com>
2023-04-14 15:47:28 +0000

    0. Minimum Operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Minimum operation
+"""
+
+def minOperations(n):
+    if n == 1:
+        return 0
+    
+    operations = 0
+    h = 1
+    while h < n:
+        if n % h == 0:
+            operations += 2
+            h *= 2
+        else:
+            operations += 1
+            h += h - n % h
+            
+    return operations
+
