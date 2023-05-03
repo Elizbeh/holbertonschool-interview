7b79913a9f54d559873dddbadd738d15a592ad9c
Elizbeh <eliz01_01@yahoo.com>
2023-05-03 12:04:40 +0000

    0. Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -2,67 +2,50 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
 /**
- * is_palindrome - function to check if a singly linked list is palindrome
- * @head: pointer to pointer to the head of a list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome 
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer to the head of the linked list
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise
  */
-
-
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = * head, * fast = *head, *prev = NULL, *next = NULL;
-    listint_t *second_half = NULL, *first_half = NULL;
-    int palindrome = 1;
-    
-    if (*head == NULL || (head) == NULL)
-        return (palindrome);
+    listint_t *current = *head;
+    int count = 0, i;
+    int *stack;
 
-    /*find the middle node of the list*/
-    while (fast != NULL && fast->next != NULL);
+    /* Count the number of nodes in the linked list */
+    while (current != NULL)
     {
-        fast = fast->next->next;
-        prev = slow;
-        slow = slow->next;
+        count++;
+        current = current->next;
     }
-    /*if  the list has an odd number of nodes, skip the middle node*/
-    if (fast != NULL)
-       slow = slow->next;
 
+    /* Allocate memory for the stack */
+    stack = malloc(sizeof(int) * count);
 
-    /* reverse the second half of the list*/
-    second_half = slow;
-    while (second_half != NULL)
+    /* Push the values of the linked list onto the stack */
+    current = *head;
+    for (i = 0; i < count; i++)
     {
-        next = second_half->next;
-        second_half->next = prev;
-        prev = second_half;
-        second_half =  next;
+        stack[i] = current->n;
+        current = current->next;
     }
-    /*comapre the first half and the reversed second half of the list*/
-    first_half = *head;
-    second_half = prev;
-    while (second_half != NULL)
+
+    /* Traverse the linked list again and compare each value with the value at the top of the stack */
+    current = *head;
+    for (i = count - 1; i >= 0; i--)
     {
-        if (first_half->n != second_half->n)
+        if (stack[i] != current->n)
         {
-            palindrome = 0;
-            break;
+            free(stack);
+            return (0);
         }
-        first_half = first_half->next;
+        current = current->next;
     }
-    /*restore the original list*/
-    second_half = prev;
-    prev = NULL;
-    while (second_half != NULL)
-    {
-        next = second_half->next;
-        second_half->next = prev;
-        prev = second_half;
-        second_half = next;
-    }
-    *head = prev;
 
-    return (palindrome);
+    /* Free the stack and return 1 (is a palindrome) */
+    free(stack);
+    return (1);
 }
+
