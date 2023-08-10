e9dbcfd3ccab25d3374144d2a87a80592b210d95
root <root@LAPTOP-J7HTNKBB>
2023-08-10 13:26:38 +0200

    0. Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 
-# A method that determines if all the boxes can be opened.
-
-
 
 def canUnlockAll(boxes):
-    # Boxes to be opened
-    boxes_opened = {0}
-    # Boxes  that needs to be opend
-    next_to_open = [0]
-    #Interattion until all boxes  have been opened
-    while len(next_to_open) > 0:
-        box = next_to_open.pop(0)
-        #Loop over the keys in the current box
-        for key in boxes[box]:
-            # Add newly open box to the queue
-            if key not in boxes_opened and key < len(boxes):
-                next_to_open.append(key)
-                boxes_opened.add(key)
-    # Return True, else return False if all boxes has been opened
-    return len(boxes_opened) == len(boxes)
-  
+    """
+    Determine if all boxes can be unlocked.
+    Given a list of boxes where each box contains
+    keys to other boxes, this function checks
+    whether it is possible to unlock all boxes
+    starting from the first one.
+    :param boxes: A list of lists, where each sublist
+                  represents a box and contains the indices
+                  of other boxes that it can unlock.
+    :return: True if all boxes can be unlocked, False otherwise.
+    """
+    total_boxes = len(boxes)
+    visited = {0}
+    keys_to_check = boxes[0]
+
+    while keys_to_check:
+        key = keys_to_check.pop()
+
+        if key not in visited:
+            visited.add(key)
+
+            keys_to_check.extend(boxes[key])
+    return len(visited) == total_boxes
