6a09f9ead6227c9269e24f64c98feb2274b749cd
root <root@LAPTOP-J7HTNKBB>
2023-08-30 18:17:51 +0200

    ....

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -6,15 +6,15 @@
  */
 void heapify_up(heap_t *node)
 {
-	while (node->parent && node->n > node->parent->n)
-	{
-		int tmp = node->n;
-
-		node->n = node->parent->n;
-		node->parent->n = tmp;
-		node = node->parent;
-	}
+    while (node->parent && node->n > node->parent->n)
+    {
+        int tmp = node->n;
+        node->n = node->parent->n;
+        node->parent->n = tmp;
+        node = node->parent;
+    }
 }
+
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
  * @root: Double pointer to the root node of the Heap
@@ -22,64 +22,48 @@ void heapify_up(heap_t *node)
  *
  * Return: Pointer to the inserted node, or NULL on failure
  */
-/**
- *heap_insert - Inserts a value into a Max Binary Heap
- *@root: Double pointer to the root node of the Heap
- *@value: Value to store in the node to be inserted
- *
- *Return: Pointer to the inserted node, or NULL on failure
- */
-
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *parent;
-	binary_tree_t *queue[100];
-	int front = 0, rear = 0;
+    heap_t *new_node, *parent;
+    binary_tree_t *queue[100];
+    int front = 0, rear = 0;
 
-	new_node = binary_tree_node(NULL, value);
-	if (!new_node)
-		return (NULL);
-	if (!*root)
-	{
-		*root = new_node;
-		return (new_node);
-	}
+    new_node = binary_tree_node(NULL, value);
+    if (!new_node)
+        return (NULL);
 
-	queue[rear++] = *root;
+    if (!*root)
+    {
+        *root = new_node;
+        return (new_node);
+    }
 
-	while (front < rear)
-	{
-		parent = queue[front];
-		front++;
+    queue[rear++] = *root;
 
-		if (!parent->left)
-		{
-			parent->left = new_node;
-			new_node->parent = parent;
-			break;
-		}
-		else if (!parent->right)
-		{
-			parent->right = new_node;
-			new_node->parent = parent;
-			break;
-		}
+    while (front < rear)
+    {
+        parent = queue[front];
+        front++;
 
-		queue[rear++] = parent->left;
-		queue[rear++] = parent->right;
-	}
+        if (!parent->left)
+        {
+            parent->left = new_node;
+            new_node->parent = parent;
+            break;
+        }
+        else if (!parent->right)
+        {
+            parent->right = new_node;
+            new_node->parent = parent;
+            break;
+        }
 
-	heapify_up(new_node);
+        queue[rear++] = parent->left;
+        queue[rear++] = parent->right;
+    }
 
-	while (new_node->parent &&
-		new_node->n > new_node->parent->n)
-	{
-		int tmp = new_node->n;
+    heapify_up(new_node);
 
-		new_node->n = new_node->parent->n;
-		new_node->parent->n = tmp;
-		new_node = new_node->parent;
-	}
-
-	return (new_node);
+    return (new_node);
 }
+
