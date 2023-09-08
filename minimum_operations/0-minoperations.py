0135d5ad1747c57120256b5c6052d67e6c48c0be
root <root@LAPTOP-J7HTNKBB>
2023-09-08 11:20:24 +0200

    PEP8 Correction

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -21,10 +21,10 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             n //= divisor
