52c49dfe01025ae3e21e19f507e193e91e2f7424
root <root@LAPTOP-J7HTNKBB>
2023-09-08 10:36:59 +0200

    0. Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,17 +5,15 @@ Minimum operation
 
 
 def minOperations(n):
-    if n == 1:
+    if n <= 1:
         return 0
-    
-    operations = 0
-    h = 1
-    while h < n:
-        if n % h == 0:
-            operations += 2
-            h *= 2
-        else:
-            operations += 1
-            h += h - n % h
 
-    return operations
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + i // j)
+
+    return operations[n] if operations[n] != float('inf') else 0
