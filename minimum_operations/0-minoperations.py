0cc997b0224b3233acaa13bbb974c875c81a2d6f
root <root@LAPTOP-J7HTNKBB>
2023-09-08 10:45:20 +0200

    Test

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,12 +8,13 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    operations = [0] * (n + 1)
+    dp = [0] * (n + 1)
 
     for i in range(2, n + 1):
-        operations[i] = float('inf')
-        for j in range(1, i):
-            if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + i // j)
+        dp[i] = i  # Start with the maximum possible value
 
-    return operations[n] if operations[n] != float('inf') else 0
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
